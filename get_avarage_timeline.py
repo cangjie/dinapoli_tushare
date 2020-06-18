e2ee611ff65f87c73761a3703afc02a0e2e31223
cangjie <cangjie@hotmail.com>
2020-06-18 13:29:11 +0800

    compute today

--- get_avarage_timeline.py
+++ get_avarage_timeline.py
@@ -69,38 +69,38 @@ def compute_avarage(formated_today):
 
 
 
-#compute_avarage(datetime.date.today().strftime('%Y%m%d'))
+compute_avarage(datetime.date.today().strftime('%Y%m%d'))
 
-compute_avarage('20200506')
-compute_avarage('20200507')
-compute_avarage('20200508')
-
-
-
-compute_avarage('20200511')
-compute_avarage('20200512')
-compute_avarage('20200513')
-compute_avarage('20200514')
-compute_avarage('20200515')
-
-compute_avarage('20200518')
-compute_avarage('20200519')
-compute_avarage('20200520')
-compute_avarage('20200521')
-compute_avarage('20200522')
+#compute_avarage('20200506')
+#compute_avarage('20200507')
+#compute_avarage('20200508')
 
 
 
-compute_avarage('20200525')
-compute_avarage('20200526')
-compute_avarage('20200527')
-compute_avarage('20200528')
-compute_avarage('20200529')
+#compute_avarage('20200511')
+#compute_avarage('20200512')
+#compute_avarage('20200513')
+#compute_avarage('20200514')
+#compute_avarage('20200515')
+
+#compute_avarage('20200518')
+#compute_avarage('20200519')
+#compute_avarage('20200520')
+#compute_avarage('20200521')
+#compute_avarage('20200522')
 
 
 
-compute_avarage('20200601')
-compute_avarage('20200602')
-compute_avarage('20200603')
-compute_avarage('20200604')
-compute_avarage('20200605')
+#compute_avarage('20200525')
+#compute_avarage('20200526')
+#compute_avarage('20200527')
+#compute_avarage('20200528')
+#compute_avarage('20200529')
+
+
+
+#compute_avarage('20200601')
+#compute_avarage('20200602')
+#compute_avarage('20200603')
+#compute_avarage('20200604')
+#compute_avarage('20200605')
