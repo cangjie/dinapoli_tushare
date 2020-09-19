2f682c8986836315b72e2af63e6e44d6b163066e
cangjie <cangjie@hotmail.com>
2020-09-19 11:50:10 +0800

    get all gid all kline days

--- get_all_k_line_day_all_gids.py
+++ get_all_k_line_day_all_gids.py
@@ -1,30 +1,35 @@
 import tushare as ts
 import sys
+import datetime
 util = __import__('util')
 kline_type = 'day'
 if (len(sys.argv) >= 2):
     kline_type = sys.argv[1].strip()
-
-
 redis = util.redis_client
 pipe = util.redis_pipe
 platform = util.get_current_os()
-
-df = ts.get_today_all()
-i = df['code'].size-1
-while (i>=0):
-    code = df['code'][i]
-    str_code = util.get_8_digit_gid(code)
+today_timestamp = str(int(datetime.datetime.now().timestamp()))
+all_gids = redis.smembers('all_gids')
+while (len(all_gids) > 0):
+    code = all_gids.pop()
+    str_code = str(code)
+    str_code = str_code.split(' ')[0].strip().replace('b\'', '')
+    print(str(len(all_gids)) + ' ' + str_code)
     str_key_name = str_code + '_kline_' + kline_type.strip()
     kline_list = redis.zrange(str_key_name, 0, -1)
-    df_kline = ts.get_hist_data(code)
+    df_kline = ts.get_hist_data(str_code[2:8])
+    if (df_kline is None):
+        continue
     str_start_date = df_kline.index[df_kline.index.size - 1]
-    if (len(kline_list) == 0):
-        j = df_kline.index.size-1
-        while(j>=0):
+    timestamp_start_date = util.get_timestamp(str_start_date, '%Y-%m-%d')
+    pipe.zremrangebyscore(str_key_name, timestamp_start_date, today_timestamp)
+    j = df_kline.index.size-1
+    while(j>=0):
             str_kline_date = df_kline.index[j]
             timestamp = util.get_timestamp(str_kline_date, '%Y-%m-%d')
-            value_str = str_kline_date
+            value_str = str_code + ',' + str_kline_date + ' 9:30:00,' + str(df_kline['open'][j]) \
+                + ',' + str(df_kline['close'][j]) + ',' + str(df_kline['high'][j]) + ',' + str(df_kline['low'][j]) \
+                + ',' + str(int(100 * float(df_kline['volume'][j]))) + ',0'
             if (platform == 'darwin' or platform == 'win32'):
                 pipe.zadd(str_key_name, value_str, timestamp)
             else:
@@ -33,6 +38,5 @@ while (i>=0):
                 else:
                     pipe.zadd(str_key_name, {value_str: timestamp})
             j = j - 1
-
-
-    i = i - 1
+    pipe.execute()
+redis.bgsave()
