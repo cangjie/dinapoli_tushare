51775686db3b3a018a781eb36f8f16d6e329c9c4
cangjie <cangjie@hotmail.com>
2020-09-20 21:43:17 +0800

    today kline

---
+++ get_today_k_line_all_gids.py
@@ -0,0 +1,40 @@
+import tushare as ts
+import sys
+import datetime
+util = __import__('util')
+kline_type = 'day'
+if (len(sys.argv) >= 2):
+    kline_type = sys.argv[1].strip()
+redis = util.redis_client
+pipe = util.redis_pipe
+platform = util.get_current_os()
+str_current_date = str(datetime.datetime.now())
+str_current_date = str_current_date.split(' ')[0]
+
+df = ts.get_today_all()
+i = 0
+while (i < df['code'].size):
+    gid = df['code'][i]
+    gid = util.get_8_digit_gid(gid)
+    str_key_name = gid + '_kline_' + kline_type
+    kline_list = redis.zrange(str_key_name, 0, -1)
+    last_index = len(kline_list)-1
+    str_last_date = str(kline_list[last_index]).split(',')[1].strip()
+    str_last_date = str_last_date.split(' ')[0]
+    if (str_current_date == str_last_date):
+        pipe.zremrangebyrank(str_key_name, last_index, last_index)
+        value_str = gid+','+str_current_date+' 09:30:00,'+str(df['open'][i])+','+str(df['trade'][i]) + ','\
+            + str(df['high'][i]) + ',' + str(df['low'][i]) + ',' + str(int(df['volume'][i])) + ',' \
+            + str(df['amount'][i])
+        timestamp = util.get_timestamp(str_current_date, '%Y-%m-%d')
+        if (platform == 'darwin' or platform == 'win32'):
+            pipe.zadd(str_key_name, value_str, timestamp)
+        else:
+            if (platform == 'linux'):
+                pipe.zadd(str_key_name, {value_str: timestamp})
+            else:
+                pipe.zadd(str_key_name, {value_str: timestamp})
+        pipe.persist(str_key_name)
+        pipe.execute()
+        value_str = ''
+    i = i + 1
