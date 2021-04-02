c0ce4149c81856701326f89bec60f438c8d55226
cangjie <cangjie@hotmail.com>
2021-04-02 15:20:19 +0800

    uni platform redis command

--- get_today_k_line_all_gids.py
+++ get_today_k_line_all_gids.py
@@ -34,16 +34,9 @@ while (i < df['code'].size):
         + str(df['amount'][i])
     timestamp = util.get_timestamp(str_current_date, '%Y-%m-%d')
     #print(timestamp)
-    if (platform == 'darwin' or platform == 'win32'):
-        pipe.zadd(str_key_name, value_str, timestamp)
-        print('zadd ' + str(timestamp) + ' ' + value_str + ' ' + platform)
-    else:
-        if (platform == 'linux'):
-            pipe.zadd(str_key_name, {value_str: timestamp})
-            print('zadd ' + str(timestamp) + ' ' + value_str + ' ' + platform)
-        else:
-            pipe.zadd(str_key_name, {value_str: timestamp})
-            print('zadd ' + str(timestamp) + ' ' + value_str + ' ' + platform)
+
+    pipe.zadd(str_key_name, {value_str: timestamp})
+    print('zadd ' + str(timestamp) + ' ' + value_str + ' ' + platform)
     pipe.persist(str_key_name)
     pipe.execute()
     value_str = ''
