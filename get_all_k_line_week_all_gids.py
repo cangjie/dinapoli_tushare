76d197f40502a7edce4d4e1a2c2b2248d60d6015
cangjie <cangjie@hotmail.com>
2021-05-21 13:55:44 +0800

    get week kline

---
+++ get_all_k_line_week_all_gids.py
@@ -0,0 +1,31 @@
+import tushare as ts
+import sys
+import datetime
+util = __import__('util')
+kline_type = 'week'
+if (len(sys.argv) >= 2):
+    kline_type = sys.argv[1].strip()
+redis = util.redis_client
+pipe = util.redis_pipe
+platform = util.get_current_os()
+today_timestamp = str(int(datetime.datetime.now().timestamp()))
+all_gids = redis.smembers('all_gids')
+while (len(all_gids) > 0):
+    code = all_gids.pop()
+    str_code = str(code)
+    str_code = str_code.split(' ')[0].strip().replace('b\'', '')
+    print(str(len(all_gids)) + ' ' + str_code)
+    str_key_name = str_code + '_kline_' + kline_type.strip()
+    redis.delete(str_key_name)
+    df_kline = ts.get_hist_data(str_code[2:8], ktype='W')
+    j = 0
+    while (j < df_kline.index.size):
+        str_kline_date = df_kline.index[j]
+        timestamp = util.get_timestamp(str_kline_date, '%Y-%m-%d')
+        value_str = str_code + ',' + str_kline_date + ' 9:30:00,' + str(df_kline['open'][j]) \
+            + ',' + str(df_kline['close'][j]) + ',' + str(df_kline['high'][j]) + ',' + str(df_kline['low'][j]) \
+            + ',' + str(int(100 * float(df_kline['volume'][j]))) + ',0'
+        pipe.zadd(str_key_name, {value_str: timestamp})
+        j = j + 1
+    pipe.execute()
+redis.bgsave()
