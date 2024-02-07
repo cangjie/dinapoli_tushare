5b8ec0c48572827f7fca49d2cb9fc47664f2ade5
cangjie <cangjie@hotmail.com>
2024-02-07 15:20:04 +0800

    get all k line day

--- get_all.py
+++ get_all.py
@@ -12,24 +12,91 @@ if (len(sys.argv) >= 2):
 redis = util.redis_client
 pipe = util.redis_pipe
 platform = util.get_current_os()
+
+def deal_df(df):
+    i = 0
+    value_str = ''
+    while(i < df.index.size):
+        code = df['ts_code'][i]
+
+        if (code[0:3] == '688'):
+            ts_code = 'kc' + code[0:6]
+        else:
+            ts_code = code[7:9].lower() + code[0:6]
+
+        trade_date = df['trade_date'][i]
+        trade_date_str = trade_date[0:4] + '-' + trade_date[4:6] + '-' + trade_date[6:8]
+        close = df['close'][i]
+        open = df['open'][i]
+        high = df['high'][i]
+        low = df['low'][i]
+        vol = df['vol'][i]
+        amount = df['amount'][i]
+        str_key_name = ts_code + '_kline_day'
+        timestamp = util.get_timestamp(trade_date, '%Y%m%d')
+        value_str = ts_code + ',' + trade_date_str + ' 9:30:00,' + str(open) \
+            + ',' + str(close) + ',' + str(high) + ',' + str(low) \
+            + ',' + str(vol) + ',' + str(amount) + ',0'
+        pipe.zadd(str_key_name, {value_str: timestamp})
+        i = i + 1
+    pipe.persist(str_key_name)
+    try:
+        pipe.execute()
+    except Exception as err:
+        print(err)
+        time.sleep(10)
+        pipe.execute()
+    finally:
+        redis.bgsave()
+
+
+
+def deal_code_list(codeList):
+    getted = False
+    while(not getted):
+        try:
+            df = pro.daily(ts_code=newCodeList, start_date=startDate, end_date=endDate)
+            deal_df(df)
+            getted = True
+        except Exception as err:
+            getted = False
+            print(err)
+            time.sleep(10)
+
+
+    print('data getted:', df.index.size)
+
+
+
 today_timestamp = str(int(datetime.datetime.now().timestamp()))
 all_gids = redis.smembers('all_gids')
 nowDate = datetime.datetime.now()
-startDate = nowDate.strftime('%Y%m%d')
-endDate = (nowDate + datetime.timedelta(days=-30)).strftime('%Y%m%d')
+endDate = nowDate.strftime('%Y%m%d')
+startDate = (nowDate + datetime.timedelta(days=-15)).strftime('%Y%m%d')
 goNext = True
+i = 0
+newCodeList = ''
 while (len(all_gids) > 0):
     if (goNext):
         code = all_gids.pop()
     str_code = str(code)
     str_code = str_code.split(' ')[0].strip().replace('b\'', '')
     newCode = str_code[2:8] + '.' + str_code[0:2].replace('kc', 'sh').upper()
+    if (newCodeList == '') :
+        newCodeList = newCode
+    else:
+        newCodeList = newCodeList + ',' + newCode
+    i = i + 1
+    if (i%400==0):
+        deal_code_list(newCodeList)
+        time.sleep(5)
+        i = 0
+        newCodeList = ''
     print(str(len(all_gids)) + ' ' + newCode)
-    try:
-        df = ts.pro_bar(ts_code=newCode, asset='E', adj='qfq', start_date=startDate, end_date=endDate)
-        goNext = True
-    except:
-        time.sleep(10)
-        goNext = False
+deal_code_list(newCodeList)
+
+
+
+
 
     #print(df)
--- get_all_k_line_day_all_gids.py
+++ get_all_k_line_day_all_gids.py
@@ -1,7 +1,10 @@
-import tushare as ts
 import sys
 import datetime
 import time
+import tushare as ts
+token = '4da2fbec9c2cee373d3aace9f9e200a315a2812dc11267c425010cec'
+ts.set_token(token)
+pro = ts.pro_api()
 util = __import__('util')
 kline_type = 'day'
 if (len(sys.argv) >= 2):
@@ -9,47 +12,91 @@ if (len(sys.argv) >= 2):
 redis = util.redis_client
 pipe = util.redis_pipe
 platform = util.get_current_os()
-today_timestamp = str(int(datetime.datetime.now().timestamp()))
-all_gids = redis.smembers('all_gids')
-while (len(all_gids) > 0):
-    code = all_gids.pop()
-    str_code = str(code)
-    str_code = str_code.split(' ')[0].strip().replace('b\'', '')
-    print(str(len(all_gids)) + ' ' + str_code)
-    str_key_name = str_code + '_kline_' + kline_type.strip()
-    #kline_list = redis.zrange(str_key_name, 0, -1)
-    df_kline = ts.get_hist_data(str_code[2:8])
-    if (df_kline is None):
-        continue
-    str_start_date = df_kline.index[df_kline.index.size - 1]
-    timestamp_start_date = util.get_timestamp(str_start_date, '%Y-%m-%d')
-    pipe.zremrangebyscore(str_key_name, timestamp_start_date, today_timestamp)
-    j = df_kline.index.size-1
-    while(j>=0):
-        str_kline_date = df_kline.index[j]
-        timestamp = util.get_timestamp(str_kline_date, '%Y-%m-%d')
-        trunover = '0'
-        try:
-            turnover = str(df_kline['turnover'][j])
-        except:
-            turnover = '0'
-        value_str = str_code + ',' + str_kline_date + ' 9:30:00,' + str(df_kline['open'][j]) \
-            + ',' + str(df_kline['close'][j]) + ',' + str(df_kline['high'][j]) + ',' + str(df_kline['low'][j]) \
-            + ',' + str(int(100 * float(df_kline['volume'][j]))) + ',0,' + turnover
+
+def deal_df(df):
+    i = 0
+    value_str = ''
+    while(i < df.index.size):
+        code = df['ts_code'][i]
+
+        if (code[0:3] == '688'):
+            ts_code = 'kc' + code[0:6]
+        else:
+            ts_code = code[7:9].lower() + code[0:6]
+
+        trade_date = df['trade_date'][i]
+        trade_date_str = trade_date[0:4] + '-' + trade_date[4:6] + '-' + trade_date[6:8]
+        close = df['close'][i]
+        open = df['open'][i]
+        high = df['high'][i]
+        low = df['low'][i]
+        vol = df['vol'][i]
+        amount = df['amount'][i]
+        str_key_name = ts_code + '_kline_day'
+        timestamp = util.get_timestamp(trade_date, '%Y%m%d')
+        value_str = ts_code + ',' + trade_date_str + ' 9:30:00,' + str(open) \
+            + ',' + str(close) + ',' + str(high) + ',' + str(low) \
+            + ',' + str(vol) + ',' + str(amount) + ',0'
         pipe.zadd(str_key_name, {value_str: timestamp})
-        j = j - 1
+        i = i + 1
     pipe.persist(str_key_name)
     try:
         pipe.execute()
     except Exception as err:
         print(err)
-        isErr = True
-        while(isErr):
+        time.sleep(10)
+        pipe.execute()
+    finally:
+        redis.bgsave()
+
+
+
+def deal_code_list(codeList):
+    getted = False
+    while(not getted):
+        try:
+            df = pro.daily(ts_code=newCodeList, start_date=startDate, end_date=endDate)
+            deal_df(df)
+            getted = True
+        except Exception as err:
+            getted = False
+            print(err)
             time.sleep(10)
-            try:
-                pipe.execute()
-                isErr = False
-            except Exception as innerErr:
-                print(innerErr)
-                isErr = True
-redis.bgsave()
+
+
+    print('data getted:', df.index.size)
+
+
+
+today_timestamp = str(int(datetime.datetime.now().timestamp()))
+all_gids = redis.smembers('all_gids')
+nowDate = datetime.datetime.now()
+endDate = nowDate.strftime('%Y%m%d')
+startDate = (nowDate + datetime.timedelta(days=-15)).strftime('%Y%m%d')
+goNext = True
+i = 0
+newCodeList = ''
+while (len(all_gids) > 0):
+    if (goNext):
+        code = all_gids.pop()
+    str_code = str(code)
+    str_code = str_code.split(' ')[0].strip().replace('b\'', '')
+    newCode = str_code[2:8] + '.' + str_code[0:2].replace('kc', 'sh').upper()
+    if (newCodeList == '') :
+        newCodeList = newCode
+    else:
+        newCodeList = newCodeList + ',' + newCode
+    i = i + 1
+    if (i%400==0):
+        deal_code_list(newCodeList)
+        time.sleep(5)
+        i = 0
+        newCodeList = ''
+    print(str(len(all_gids)) + ' ' + newCode)
+deal_code_list(newCodeList)
+
+
+
+
+
+    #print(df)
