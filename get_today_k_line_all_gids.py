b6187bc8678e4c3227bd0d47eddac3d4a9ffb904
苍杰 <cangjie@hotmail.com>
2024-02-11 23:18:39 +0800

    h

--- get_today_k_line_all_gids.py
+++ get_today_k_line_all_gids.py
@@ -29,7 +29,7 @@ while (i < df['code'].size):
     if (str_current_date == str_last_date):
         pipe.zremrangebyrank(str_key_name, last_index, last_index)
 
-    value_str = gid+','+str_current_date+' 09:30:00,'+str(df['open'][i])+','+str(df['trade'][i]) + ','\
+    value_str = gid+','+str_current_date+' 9:30:00,'+str(df['open'][i])+','+str(df['trade'][i]) + ','\
         + str(df['high'][i]) + ',' + str(df['low'][i]) + ',' + str(int(df['volume'][i])) + ',' \
         + str(df['amount'][i]) + ',0'
     timestamp = util.get_timestamp(str_current_date, '%Y-%m-%d')
