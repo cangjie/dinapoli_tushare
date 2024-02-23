a69a2d93b5ebd28a0f14207337cbc7edbe224bce
cangjie <cangjie@hotmail.com>
2024-02-23 17:39:27 +0800

    get money

--- get_timeline.py
+++ get_timeline.py
@@ -25,8 +25,9 @@ if (len(sys.argv) >= 3):
 
 if (api == ''):
     gidArr = redis.smembers('all_gids')
-elif (api[0:2] == 'sh' or api[0:2] == 'sz'):
+elif (api.find('/') < 0 ):
     gidArrAll = redis.smembers('all_gids')
+    i = 0
     for i in range(0, len(gidArrAll)):
         gid = gidArrAll.pop().decode('utf-8').split(' ')[0]
         if (gid.startswith(api)):
@@ -49,18 +50,29 @@ else:
         gidArr.append(itemList[i]['gid'])
         i = i + 1
 
+threadNum = min(threadNum, len(gidArr))
+
 class Result:
     def __init__(self):
         self.gid = ''
         self.lines = []
+        self.linesToSave = []
+        self.try_times = 0
+        self.finish = False
+        self.saved = False
+        self.computing = False
 
 def deal_result(result):
+    print(result.gid + ' compute start\r\n')
+    result.linesToSave = []
+    result.computing = True
     lines = result.lines
     key = result.gid + '_money'
     startScore = util.get_timestamp(nowDate.strftime('%Y%m%d'), '%Y%m%d')
     endScore = util.get_timestamp(nowDate.strftime('%Y%m%d') + ' 23:59:59', '%Y%m%d %H:%M:%S')
-    tickArr = redis.zrange(key, startScore, endScore, byscore==True)
+    tickArr = redis.zrange(key, startScore, endScore, byscore=True)
     #tickArr = redis.zrange
+    j = 0
     for j in range(0, len(tickArr)):
         tick = tickArr[j].decode('utf-8')
 
@@ -75,6 +87,7 @@ def deal_result(result):
     smallDeal = 0
     smallNetFlow = 0
     lastLineTimeStamp = 0
+    j = 0
     for j in range(0, len(lines)):
         lineArr = lines[j].split(',')
         if (len(lineArr) != 6):
@@ -108,8 +121,8 @@ def deal_result(result):
             needSave = True
             saveTimeStr = nowDate.strftime('%Y-%m-%d 11:00:00')
             saveTimeStamp = util.get_timestamp(saveTimeStr, '%Y-%m-%d %H:%M:%S')
-        if (lastLineTimeStamp < util.get_timestamp(nowDate.strftime('%Y%m%d') + ' 11:30:00', '%Y%m%d %H:%M:%S') \
-                and currentLineTimeStamp >= util.get_timestamp(nowDate.strftime('%Y%m%d') + ' 11:30:00',
+        if (lastLineTimeStamp <= util.get_timestamp(nowDate.strftime('%Y%m%d') + ' 11:30:00', '%Y%m%d %H:%M:%S') \
+                and currentLineTimeStamp > util.get_timestamp(nowDate.strftime('%Y%m%d') + ' 11:30:00',
                                                                '%Y%m%d %H:%M:%S')):
             needSave = True
             saveTimeStr = nowDate.strftime('%Y-%m-%d 11:30:00')
@@ -147,7 +160,8 @@ def deal_result(result):
             saveStr = saveTimeStr + ',' + str(hugeDeal) + ',' + str(hugeNetFlow) + ','
             saveStr = saveStr + str(bigDeal) + ',' + str(bigNetFlow) + ',' + str(midDeal) + ',' + str(midNetFlow) + ','
             saveStr = saveStr + str(smallDeal) + ',' + str(smallNetFlow)
-            pipe.zadd(key, {saveStr: saveTimeStamp})
+            #pipe.zadd(key, {saveStr: saveTimeStamp})
+            result.linesToSave.append(saveStr)
             hugeDeal = 0
             hugeNetFlow = 0
             bigDeal = 0
@@ -156,7 +170,7 @@ def deal_result(result):
             midNetFlow = 0
             smallDeal = 0
             smallNetFlow = 0
-            pipe.zadd(key, {saveStr: saveTimeStamp})
+            #pipe.zadd(key, {saveStr: saveTimeStamp})
 
         volume = int(lineArr[3])
         if (volume >= 2000):
@@ -184,11 +198,15 @@ def deal_result(result):
             if (lineArr[5] == 'D'):
                 smallNetFlow = smallNetFlow - volume
         lastLineTimeStamp = currentLineTimeStamp
+    result.finish = True
+    result.computing = False
+    print(result.gid + ' compute end\r\n')
 
 
 def main_thread():
     while(len(gidArr)>0 or len(results) > 0):
         if (len(gidArr) > 0):
+            i = 0
             for i in range(0, threadNum):
                 if ((len(threads) == i or not threads[i].is_alive()) and len(gidArr) > 0):
                     try:
@@ -200,15 +218,79 @@ def main_thread():
                         threads.append(t)
                     else:
                         threads[i] = t
+                    result = Result()
+                    result.gid = gid
+                    result.lines = []
+                    results.append(result)
                     t.start()
+                    #t.join()
                 #t.join()
-        time.sleep(10)
+
+        #haveSthToCompute = False
+
+        #i = 0
+        #for i in range(0, len(results)):
+        #    try:
+        #        result = results[i]
+        #        if (len(result.lines) == 0 or  result.finish or result.computing):
+        #            continue
+        #        haveSthToCompute = True
+        #        t = threading.Thread(target=deal_result, args=(result,))
+
+        #        t.start()
+                #deal_result(result)
+                #results.remove(result)
+                #key = result.gid + '_money'
+                #pipe.execute()
+                #pipe.persist(key)
+                #print(result.gid + ' saved')
+        #    except Exception as err:
+        #        print(str(err))
+        #        break
+        #if (not haveSthToCompute):
+        #    i = 0
+        #    for i in range(0, len(results)):
+        #        results[i].try_times = results[i].try_times + 1
+        #    time.sleep(1)
+        time.sleep(threadNum*2)
+        haveSthToSave = False
+        i = 0
         for i in range(0, len(results)):
-            result = results.pop()
-            deal_result(result)
-            key = result.gid + '_money'
-            pipe.execute()
-            pipe.persist(key)
+            try:
+                result = results[i]
+                if (not result.finish or result.saved):
+                    continue
+                haveSthToSave = True
+                key = result.gid + '_money'
+                j = 0
+                for j in range(0, len(result.linesToSave)):
+                    timeStr = result.linesToSave[j].split(',')[0]
+                    timeStamp = util.get_timestamp(timeStr, '%Y-%m-%d %H:%M:%S')
+                    pipe.zadd(key, {result.linesToSave[j]: timeStamp})
+
+                pipe.execute()
+                pipe.persist(key)
+                print(result.gid + ' saved')
+                result.saved = True
+            except Exception as err:
+                print(err)
+                try:
+                    result.saved = True
+                except Exception as err:
+                    print('remove err ' + str(err))
+        if (not haveSthToSave):
+            i = 0
+            for i in range(0, len(results)):
+                results[i].try_times = results[i].try_times + 1
+            time.sleep(1)
+        i = 0
+        while(len(results) > 0 and i < len(results)):
+            result = results[i]
+            if (result.try_times > 100 or (result.finish and result.saved)):
+                results.remove(result)
+            else:
+                i = i + 1
+
 
         try:
             redis.bgsave()
@@ -230,13 +312,16 @@ def main_thread():
 
 
 def get_tick(gid):
-    print(gid + ' start')
+
+    print(gid + ' start\r\n')
+
     if (gid[0:2] == 'kc'):
         newGid = gid[2:8] + '.SH'
     else:
         newGid = gid[2:8] + '.' + gid[0:2].upper()
     df = ts.realtime_tick(ts_code = newGid)
     lines = []
+    i = 0
     for i in range(0, df.index.size):
         type = 'E'
         if (df['TYPE'][i] == '买盘'):
@@ -245,11 +330,14 @@ def get_tick(gid):
             type = 'D'
         valueStr = df['TIME'][i] + ',' + str(df['PRICE'][i]) + ',' + str(df['CHANGE'][i]) + ',' + str(df['VOLUME'][i]) + ',' + str(df['AMOUNT'][i]) + ',' + type
         lines.append(valueStr)
-    result = Result()
-    result.gid = gid
-    result.lines = lines
-    results.append(result)
-    print(gid + ' end')
+    i = 0
+    for i in range(0, len(results)):
+        if (results[i].gid == gid):
+            results[i].lines = lines
+            deal_result(results[i])
+            break
+    print(gid + ' end\r\n')
+
 
 
 threads = []
